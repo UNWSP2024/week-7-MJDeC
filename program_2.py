b9ebd0e6f5bef5bcca043f903011bb41292d6a58
MJDeC <mjdecaro@students.unwsp.edu>
2024-10-15 19:54:37 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -30,8 +30,10 @@ def main():
 def display_larger_than_n_list(n, n_list):
     # Write your code to display all of the numbers in the list that are greater than then number n. below
     print('In display_larger_than_n_list')
-    greater=n>number in number_list
-        print(greater)
+    n=input(float('Enter a number between 1 and 10.'))
+    for x in number_list>n:
+            x=bigger_numbers
+    
 # Call the main function.
 if __name__ == '__main__':
     main()
