771cea597ce8aba4ee709644a781ef446fda4fe7
MJDeC <mjdecaro@students.unwsp.edu>
2024-10-17 13:25:07 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -30,7 +30,7 @@ def main():
 def display_larger_than_n_list(n, n_list):
     # Write your code to display all of the numbers in the list that are greater than then number n. below
     print('In display_larger_than_n_list')
-    for x in number_list>n:
+    for x in n_list>n:
         bigger_numbers=x
         print(bigger_numbers)
     
